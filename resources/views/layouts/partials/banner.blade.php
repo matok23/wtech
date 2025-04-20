de3e2eb52524d794f6cf51b4bc60d9df21b7f402
JanaPuchein <jana.pucheinova@gmail.com>
2025-04-20 21:44:04 +0200

    skareda nepodarena responzivita na oko OPRAVIT

--- resources/views/layouts/partials/banner.blade.php
+++ resources/views/layouts/partials/banner.blade.php
@@ -1,3 +1,3 @@
 <div class="banner container-fluid">
-    <img src="/pictures/bannerImg.png" alt="Banner Image">
+    <img src="/pictures/bannerImg.png" alt="Banner Image" class="responsive-banner">
 </div>
